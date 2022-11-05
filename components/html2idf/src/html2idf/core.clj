d5cef0a467227088c45e3fb2268040d33d3505f7
RJ Sheperd <rjsheperd@gmail.com>
2022-11-04 17:56:15 -0700

    Remove cruft/clean up

--- components/html2idf/src/html2idf/core.clj
+++ components/html2idf/src/html2idf/core.clj
@@ -1,13 +1,9 @@
 (ns html2idf.core
   (:require [clojure.string :as str]
-            [clojure.walk   :as walk]
             [clojure.java.io :as io]
             [hickory.core   :as hickory]))
 
-(defn html->hiccup [html]
-  (first (hickory/as-hiccup (hickory/parse html))))
-
-(def the-html "<html><body style=\"color: red;\"><div>Hello World</div></body></html>")
+;; Constants
 
 (def stop-words (-> "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
                     (str/split #",")
@@ -17,11 +13,12 @@
   "regular expression for finding all non-word characters and leaving spaces for delimiting"
   #"(?![a-zA-Z0-9À-ÿ\s])(\W)")
 
-(first stop-words)
+;; Helper Fns
 
-(hickory/as-hiccup (hickory/parse the-html))
+(defn- html->hiccup [html]
+  (first (hickory/as-hiccup (hickory/parse html))))
 
-(defn walk-hiccup-content [fn [tag props & children]]
+(defn- walk-hiccup-content [fn [_tag _props & children]]
   (doseq [child children]
     (cond
       (string? child)
@@ -42,19 +39,16 @@
                                                                 (str/replace #"[0-9]" "")
                                                                 (str/split #"\s")))))
 
-(defn calculate-tf [terms]
+(defn- calculate-tf [terms]
   (let [c (count terms)
         m (frequencies terms)]
-    (reduce-kv (fn [n k v] (assoc n k (float (/ v c)))) {} m)))
+    (persistent! (reduce-kv (fn [n k v] (assoc! n k (float (/ v c)))) (transient {}) m))))
 
-(empty {:hello "world"})
+;; Public Fns
 
-(assoc {} "hello" 1)
-
-(update {"hello" 0} "hello" inc)
-(html->terms
- (slurp (io/file "../../../behave6/src/DocFolder/en_US/Html/CBD_Dougfir_Lodgepole.html")))
-
-(def the-terms *1)
-
-(sort-by val > (calculate-tf the-terms))
+(defn html->tf [file]
+  (->> file
+       (io/file)
+       (slurp)
+       (html->terms)
+       (calculate-tf)))
