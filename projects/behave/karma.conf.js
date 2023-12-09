bdca07b2f7e794a7f25c3b1770804ad575c06e66
RJ Sheperd <rjsheperd@gmail.com>
2023-12-08 17:59:41 -0800

    Fix deps for Behave build, tests are running

---
+++ projects/behave/karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    browserNoActivityTimeout: 3000,
+    basePath: 'out',
+    files: ['karma-tests.js'],
+    frameworks: ['cljs-test'],
+      plugins: ['karma-cljs-test', 'karma-chrome-launcher', 'karma-junit-reporter'],
+    colors: true,
+    logLevel: config.LOG_INFO,
+    client: {args: ["shadow.test.karma.init"], singleRun: true},
+
+    // JUnit Report
+    reporters: ['progress', 'junit'],
+    // the default configuration
+    junitReporter: {
+      outputDir: '', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true, // add browser name to report and classes names
+      // nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      // classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: 1 // use '1' if reporting to be per SonarQube 6.2 XML format
+    }
+  })
+};
