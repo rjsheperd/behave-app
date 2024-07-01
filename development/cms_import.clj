33ae65af0e2b092f8d5b3a4eb8eee641ccdd12f7
RJ Sheperd <rjsheperd@gmail.com>
2024-07-01 14:51:41 -0700

    Update CMS Import for Datomic

--- development/cms_import.clj
+++ development/cms_import.clj
@@ -3,12 +3,13 @@
    [clojure.edn :refer [read-string]]
    [clojure.java.io :as io]
    [clojure.set :refer [rename-keys]]
-   [datahike-store.main :as ds]
-   [datahike.api :as d]
+   [datomic-store.main :as ds]
+   [datomic.api :as d]
    [datascript.core :refer [squuid]]
    [string-utils.interface :refer [->str]]
    [datom-utils.interface :refer [safe-deref unwrap]]
-   [me.raynes.fs :as fs]))
+   [me.raynes.fs :as fs]
+   [nano-id.core :refer [nano-id]]))
 
 (defn dissoc-in [m keys]
   (update-in m (butlast keys) dissoc (last keys)))
@@ -33,30 +34,36 @@
 
     (write-pprint-edn merged-edn (str "cms-exports/" out-file-name))))
 
-(defn ->class [[class-name methods]]
+(defn ->class [db [class-name methods]]
   (let [->param            (fn [i p] (merge {:cpp.parameter/order i
-                                             :bp/uuid             (str (squuid))}
+                                             :bp/uuid             (str (squuid))
+                                             :bp/nid              (nano-id)}
                                             (rename-keys p {:id   :cpp.parameter/name
                                                             :type :cpp.parameter/type})))
         ->fn               (fn [[_ {:keys [type id parameters]}]]
                              (merge {:bp/uuid                (str (squuid))
+                                     :bp/nid                 (nano-id)
                                      :cpp.function/name      id
                                      :cpp.function/parameter (vec (map-indexed ->param parameters))}
                                     (when type {:cpp.function/return-type type})))
         has-name?          (comp some? :cpp.function/name)
-        existing-fn?       #(some?
-                             (d/q '[:find ?e .
-                                    :in $ ?fn-name
-                                    :where
-                                    [?e :cpp.function/name ?fn-name]]
-                                  @@ds/conn
-                                  (->str (:cpp.function/name %))))
         existing-class-eid (d/q '[:find ?e .
                                   :in $ ?class-name
                                   :where
                                   [?e :cpp.class/name ?class-name]]
-                                @@ds/conn
-                                (->str class-name))]
+                                db
+                                (->str class-name))
+        existing-fn?       #(and
+                             (some? existing-class-eid)
+                             (some?
+                              (d/q '[:find ?f .
+                                     :in $ ?c ?fn-name
+                                     :where
+                                     [?c :cpp.class/function ?f]
+                                     [?f :cpp.function/name ?fn-name]]
+                                   db
+                                   existing-class-eid
+                                   (->str (:cpp.function/name %)))))]
     (cond-> {:cpp.class/name     (->str class-name)
              :cpp.class/function (vec (->> (filter has-name? (map ->fn methods))
                                            (remove existing-fn?)))}
@@ -71,14 +78,14 @@
   (d/q '[:find ?e .
          :in $ ?name
          :where [?e :cpp.namespace/name ?name]]
-       (safe-deref conn) ns-name))
+       (ds/unwrap-db conn) ns-name))
 
 (defn add-export-file-to-conn [f conn]
   (let [source-edn (read-string (slurp (fs/expand-home f)))
         namespaces (reduce (fn [acc ns] (assoc acc ns (lookup-ns-id (->str ns) conn))) {} (keys source-edn))
         tx         (mapv (fn [[ns-key ns-id]]
                            (merge {:cpp.namespace/name  (->str ns-key)
-                                   :cpp.namespace/class (mapv ->class (get source-edn ns-key))}
+                                   :cpp.namespace/class (mapv (partial ->class (ds/unwrap-db conn)) (get source-edn ns-key))}
                                   (when ns-id {:db/id ns-id})))
                          namespaces)]
     (d/transact conn tx)))
@@ -162,27 +169,30 @@
                :to-key           :SIGSlopeTool})
 
   ;;; Add exports to CMS db
-  (require '[behave-cms.server :refer [init-datahike!]])
+  (require '[behave-cms.server :as cms])
 
   ;; Init Datahike Connection
-  (init-datahike!)
+  (cms/init-db!)
 
   ;; Check connection is good
-  @ds/conn
+  (def conn @ds/datomic-conn)
+
+  (def db (d/db conn))
 
   ;; Verify that global namespace exists
-  (lookup-ns-id "global" @ds/conn)
+  (lookup-ns-id "global" db)
 
   ;; Commit exports
-  (add-export-file-to-conn "./cms-exports/SIGSurface.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGMoistureScenarios.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGCrown.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGBehaveRun.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGMortality.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGIgnite.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGFineDeadFuelMoistureTool.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/SIGSlopeTool.edn" @ds/conn)
-  (add-export-file-to-conn "./cms-exports/VaporPressureDeficitCalculator.edn" @ds/conn)
+  (add-export-file-to-conn "./cms-exports/SIGSurface.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGSurface.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGMoistureScenarios.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGCrown.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGBehaveRun.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGMortality.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGIgnite.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGFineDeadFuelMoistureTool.edn" conn)
+  (add-export-file-to-conn "./cms-exports/SIGSlopeTool.edn" conn)
+  (add-export-file-to-conn "./cms-exports/VaporPressureDeficitCalculator.edn" conn)
 
   ;; Verify that SIGSurface exists
   (sort (d/q '[:find [?c-name ...]
@@ -191,7 +201,7 @@
                [?e :cpp.namespace/name "global"]
                [?e :cpp.namespace/class ?c]
                [?c :cpp.class/name ?c-name]]
-             (safe-deref @ds/conn)))
+             db))
 
   ;; Verify that SIGSurface functions exist
   (sort (d/q '[:find [?f-name ...]
@@ -199,7 +209,7 @@
                [?c :cpp.class/name "SIGSurface"]
                [?c :cpp.class/function ?f]
                [?f :cpp.function/name ?f-name]]
-             (safe-deref @ds/conn)))
+             db))
 
   (defn class-to-remove [class-name]
     (d/q '[:find ?c ?f ?p
@@ -209,7 +219,7 @@
            [?c :cpp.class/name ?class-name]
            [?c :cpp.class/function ?f]
            [?f :cpp.function/parameter ?p]]
-         (safe-deref @ds/conn) class-name))
+         db class-name))
 
   (defn retract [id]
     [:db/retractEntity id])
@@ -226,12 +236,13 @@
 
   (def remove-tx (map #(retract (ffirst (class-to-remove %))) class-names-to-remove))
 
-  (d/transact (unwrap @ds/conn) remove-tx)
+  (d/transact @ds/conn remove-tx)
 
   (d/q '[:find ?e .
          :in $ ?name
          :where
          [?e :cpp.class/name ?name]]
+       db
        @@ds/conn
        "SIGSurface")
 
