b17406aca7469fc32c2f6f1f81d28a93e3c26c33
RJ Sheperd <rjsheperd@gmail.com>
2024-07-26 08:27:48 -0700

    Update migration with temp. :db/id's, comment blocks around TX

--- development/migrations/2024_07_17_add_active_crown_flame_length_variable.clj
+++ development/migrations/2024_07_17_add_active_crown_flame_length_variable.clj
@@ -6,7 +6,7 @@
            '[datomic.api              :as d]
            '[datomic-store.main       :as ds]
            '[schema-migrate.interface :as sm]
-           '[cms-import               :refer [add-export-file-to-conn ->class]])
+           '[cms-import               :refer [add-export-file-to-conn]])
 
   (cms/init-db!)
 
@@ -26,7 +26,8 @@
     (-> (sm/->subgroup fire-behavior-input-group
                        "Active Crown Flame Length (Hidden)"
                        "behaveplus:crown:input:spotting:fire_behavior:active_crown_flame_length_hidden")
-        (merge {:group/conditionals-operator :or
+        (merge {:db/id                       -1
+                :group/conditionals-operator :or
                 :group/conditionals          [(sm/->module-conditional :equal ["surface" "crown"])]})))
 
   ;; Add Group Variable to 'Active Crown Flame Length (Hidden)' with new `SIGSpot::setActiveCrownLength` fn
@@ -35,10 +36,11 @@
     (sm/name->eid conn :variable/name "Active Crown Flame Length"))
 
   (def new-group-variable-tx
-    (-> (sm/->group-variable [:bp/uuid (:bp/uuid new-subgroup-tx)]
+    (-> (sm/->group-variable (:db/id new-subgroup-tx)
                              active-crown-flame-length-variable
                              (str (:group/translation-key new-subgroup-tx) ":active_crown_flame_length"))
-        (merge {:group-variable/cpp-namespace (sm/cpp-ns->uuid conn "global")
+        (merge {:db/id                        -2
+                :group-variable/cpp-namespace (sm/cpp-ns->uuid conn "global")
                 :group-variable/cpp-class     (sm/cpp-class->uuid conn "SIGSpot")
                 :group-variable/cpp-function  (sm/cpp-fn->uuid conn "setActiveCrownFlameLength")
                 :group-variable/cpp-parameter (sm/cpp-param->uuid conn "setActiveCrownFlameLength" "flameLength")})))
@@ -48,10 +50,14 @@
     (sm/t-key->eid db "behaveplus:crown:output:fire_type:fire_behavior:flame_length"))
 
   (def link-tx
-    (sm/->link crown-final-flame-length [:bp/uuid (:bp/uuid new-group-variable-tx)]))
+    (sm/->link crown-final-flame-length (:db/id new-group-variable-tx)))
 
-  (def tx (d/transact @ds/datomic-conn [new-subgroup-tx new-group-variable-tx link-tx]))
+  (comment
+    (def tx (d/transact @ds/datomic-conn [new-subgroup-tx new-group-variable-tx link-tx]))
+    )
 
-  #_(sm/rollback-tx! @ds/datomic-conn tx)
+  (comment
+    (sm/rollback-tx! @ds/datomic-conn tx)
+    )
 
   )
