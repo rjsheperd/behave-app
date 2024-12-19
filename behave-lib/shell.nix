df9de205180b9c314eec3ba71692f2b9201dacb6
RJ Sheperd <rjsheperd@gmail.com>
2024-12-19 14:16:28 -0800

    Enable building Behave WASM with NIX

---
+++ behave-lib/shell.nix
@@ -0,0 +1,24 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # Docs
+    doxygen
+
+    # Building/Tests
+    gnumake
+    cmake
+    llvm
+    clang
+    emscripten
+    fd
+  ];
+
+  shellHook = ''
+  export EM_CACHE = "$PWD/.em_cache"
+  export WEB_IDL=$(fd webidl_binder.py /nix/store)
+  '';
+}
