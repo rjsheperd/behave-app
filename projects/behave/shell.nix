2dc78f12cf101b3b35f75fceda0e531d50bc2d16
RJ Sheperd <rjsheperd@gmail.com>
2024-12-19 16:11:49 -0800

    Add nix-shell, bb tasks for building Desktop UberJAR

---
+++ projects/behave/shell.nix
@@ -0,0 +1,16 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # Tools
+    git
+
+    # Clojure
+    openjdk
+    clojure
+    babashka
+  ];
+}
