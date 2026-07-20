15d57d57af392d63b5d9c2340c83fd0d73f1e391
RJ Sheperd <rjsheperd@gmail.com>
2026-07-20 12:44:51 -0700

    Update .dir-locals.el to use shadow-cljs, simplify zip-extras args

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,11 @@
 ((nil . ((cider-clojure-cli-aliases . ":dev:behave/app")
-         (cider-default-cljs-repl . figwheel-main))))
+         (cider-default-cljs-repl . figwheel-main)))
+
+ ("projects/behave" . ((nil . ((cider-preferred-build-tool . shadow-cljs)
+                               (cider-default-cljs-repl . shadow)
+                               (cider-shadow-default-options . "browser")
+                               (cider-shadow-cljs-command . "bun shadow-cljs")
+                               (cider-clojure-cli-aliases . nil))))))
 
 ;; VMS Configuration
 ;; TODO: Fix to avoid having two separate aliases for projects
